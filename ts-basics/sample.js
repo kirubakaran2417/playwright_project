89444a91fa30f8b24074cb4a2e6af6716ae9af2e
Ponmalar <Ponmalar@demo.com>
2026-04-19 08:09:43 +0530

    day 2

---
+++ ts-basics/sample.js
@@ -0,0 +1,6 @@
+"use strict";
+///simple function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
---
+++ ts-basics/sample.ts
@@ -0,0 +1,5 @@
+///simple function to add two numbers
+function add(a: number, b: number): number {
+    return a + b;
+}
+console.log(add(1, 2));
